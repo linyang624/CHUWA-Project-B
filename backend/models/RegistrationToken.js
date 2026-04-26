45c39736adabf88dbac31b89e82fec68c39b3956
jz450 <jieying.zhang@duke.edu>
2026-04-25 18:00:32 -0700

    set RegistrationToken model

--- backend/models/RegistrationToken.js
+++ backend/models/RegistrationToken.js
@@ -1,7 +1,55 @@
 /* 
-    Generation Register Link
+    Generation Registration Link
     Record sending information
     3hr expiration
     !used 
+    ** This is Registration link record
 */
 
+import mongoose from 'mongoose';
+
+const registrationTokenSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        token: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        registrationLink: {
+            type: String,
+            required: true,
+        },
+        expiresAt: {
+            type: Date,
+            required: true,
+        },
+        used: {
+            type: Boolean,
+            required: false,
+        },
+        status: {
+            type: String,
+            enum: ["sent", 'submitted'],
+            default: "sent",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const RegistrationToken = mongoose.model('RegistrationToken', registrationTokenSchema);
+
+export default RegistrationToken;
