f0d4188e76e3efe9eeea46097b2a5bed14e14f7d
jz450 <jieying.zhang@duke.edu>
2026-04-28 16:53:00 -0700

    update store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import hrReducer from "../features/hr/hrSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    hr: hrReducer,
   },
 });
