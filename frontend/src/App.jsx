4cc08ddb60a061820a1ca07f3a4975658da67149
qzsgmhahaha <yanglin1999624@163.com>
2026-04-29 01:05:53 -0500

    Fix App merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,8 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import LoginPage from "./pages/employee/LoginPage";
-import RegisterPage from "./pages/employee/RegisterPage";
-import OnboardingApplicationPage from "./pages/employee/OnboardingApplicationPage";
-import PersonalInformationPage from "./pages/employee/PersonalInformationPage";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import VisaStatusPage from "./pages/employee/VisaStatusPage";
-
-export default function App() {
 import ErrorBoundary from "./components/common/ErrorBoundary";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register/:token" element={<RegisterPage />} />
-      <Route path="/visa-status" element={<VisaStatusPage />} />
-
-      <Route
-        path="/onboarding"
-        element={
-          <ProtectedRoute>
-            <OnboardingApplicationPage />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/personal-info"
-        element={
-          <ProtectedRoute>
-            <PersonalInformationPage />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
-  );
-}
     <ErrorBoundary>
       <AppRoutes />
     </ErrorBoundary>
